45260f9709adfd0dc1f700c350f42626d3e46a7c
shanajasila <shanajasilam@gmail.com>
2022-12-08 12:54:32 +0530

    search & delete

--- src/main/java/com/example/PatientApp/dao/PatientDao.java
+++ src/main/java/com/example/PatientApp/dao/PatientDao.java
@@ -1,8 +1,21 @@
 package com.example.PatientApp.dao;
 
 import com.example.PatientApp.model.Patient;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PatientDao extends CrudRepository<Patient,Integer> {
+    @Query(value = "SELECT `id`, `adress`, `contactno`, `dateofappoinment`, `doctorname`, `patientname` FROM `employees` WHERE `patientname`= :patientname",nativeQuery = true)
+    List<Patient> searchPatient(@Param("patientname") String patientname);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `employees` WHERE `id`= :id",nativeQuery = true)
+    void deletePatient(@Param("id")Integer id);
 
 }
